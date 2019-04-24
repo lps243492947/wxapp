ba011a515f48c70f49bc5020735f7a6b188ad036
lps243492947 <243492947@qq.com>
2019-04-24 14:27:29 +0800

    new wxapp

---
+++ lol/pages/legends/legends.js
@@ -0,0 +1,26 @@
+Page({
+    data: {
+        legends: [
+            {
+                name: '蔡徐坤',
+                title: '唱',
+                src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556195648&di=560255c62f94bc24ca0589f21708482d&imgtype=jpg&er=1&src=http%3A%2F%2Fdingyue.nosdn.127.net%2FoagNzd5xeYoDLPHOeDlvXGqQoYFLcHhZV9AGEBD2cmD971555242521672compressflag.jpeg%3FimageView'
+            },
+            {
+                name: '蔡徐坤',
+                title: '跳',
+                src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556195692&di=f4410b36ef4b528a019f2391a0d2219a&imgtype=jpg&er=1&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20190414%2Fbeeed30db92846749a290a40798d8a74.gif'
+            },
+            {
+                name: '蔡徐坤',
+                title: 'rap',
+                src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556195721&di=a0c5cb78770dd6904b6718ddef2982e7&imgtype=jpg&er=1&src=http%3A%2F%2Finews.gtimg.com%2Fnewsapp_match%2F0%2F8572950211%2F0'
+            },
+            {
+                name: '蔡徐坤',
+                title: '篮球',
+                src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555601001457&di=51b92273b36b62c1929c13f9457e2d79&imgtype=0&src=http%3A%2F%2Fi1.hdslb.com%2Fbfs%2Farchive%2F57b0a811777a40cc8bb7aa92c20f7fa9ae4e0e72.jpg'
+            }
+        ]
+    }
+})
